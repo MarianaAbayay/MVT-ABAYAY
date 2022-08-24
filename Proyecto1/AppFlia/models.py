e7e03914cd0a501b5d85a13aac4f621294516743
Mariana Abayay <marianaabayay@gmail.com>
2022-08-24 02:11:56 -0300

    18.Modelo. Error python check app

---
+++ Proyecto1/AppFlia/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Familiar(models.Model):
+    nombre=models.CharField(max_length=100)
+    parentesco=models.CharField(max_length=100)
+    edad=models.IntegerField()
+    fecha_cumple=models.DateField()
+
+    
+
+
--- Proyecto2/AppFlia/models.py
+++
@@ -1,5 +0,0 @@
-#MODELO 1
-
-from django.db import models
-
-# Create your models here.
