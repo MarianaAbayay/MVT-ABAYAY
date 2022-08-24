97c10d2f16f3f28cb95e55bd0be5342a8fd2a4ec
Mariana Abayay <marianaabayay@gmail.com>
2022-08-24 01:20:47 -0300

    17. template(1) para familiares

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,10 +1,17 @@
 #CONTROLADOR
 
 from django import HttpResponse
+from django.template import Context, Template
+
 
 def inicio(request):
     return HttpResponse("Bienvenidxs")
 
 def familiares(request):
-    return HttpResponse("FAMILIARES")
+    mihtml=open("Proyecto1/Plantillas/template1.html")
+    plantilla=Template(mihtml.read())
+    mihtml.close()
+    contexto=Context()
+    documento=plantilla.render(contexto)
+    return HttpResponse(documento)
     
